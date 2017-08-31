b54951182c73698c0bbafd37aa30e334c2ed3900
Jan Brezina <jan.brezina@tul.cz>
2017-08-31 18:53:31 +0200

    Binding tested.

--- src/python_bih_tree.cc
+++ src/python_bih_tree.cc
@@ -7,33 +7,57 @@
 
 #include <pybind11/pybind11.h>
 //#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
 #include <bounding_box.hh>
-//#include <bih_tree.hh>
+#include <bih_tree.hh>
 
 namespace py = pybind11;
+using namespace std;
 
+
+
+// Python binding BoundingBox wrapper
+/*
+class AABB : public BoundingBox
+{
+    AABB()
+};
+
+*/
+
+std::vector<unsigned int> tree_find_point(BIHTree *tree, Point &point, bool full_list) {
+    std::vector<unsigned int> result;
+    tree->find_point(point, result, full_list);
+    return result;
+}
+
+std::vector<unsigned int> tree_find_box(BIHTree *tree, BoundingBox &box, bool full_list) {
+    std::vector<unsigned int> result;
+    tree->find_bounding_box(box, result, full_list);
+    return result;
+}
 
 
 PYBIND11_PLUGIN(bih) {
     py::module m( "bih", "Bounding Inteval Hierarchy of Axes Aligned Bounding Boxes.");
-    py::class_<BoundingBox>(m, "AABB");
-//        .def(py::init<const vector<Point> &>())
-//        .def("min", (const Point&(BoundingBox::*)() const) &BoundingBox::min , py::return_value_policy::reference)  // need conversion to numpy array
-//        .def("max", (const Point&(BoundingBox::*)() const) &BoundingBox::max , py::return_value_policy::reference)  // need conversion to numpy array
-//        .def("size", &BoundingBox::size)
-//        .def("center", &BoundingBox::center) /// return Point, must convert to numpy array
-//        .def("projection_center", &BoundingBox::projection_center,
-//                "Return center of projection of the bounding box to given @p axis."
-//                "Axis coding is: 0 - axis x, 1 - axis y, 2 - axis z.")
-//        .def("contains_point", &BoundingBox::contains_point,        // need conversion of argument
-//                "Returns true is the  box element contains point.")
-//
-//        .def("intersect", &BoundingBox::intersect,                  // hopefully correct
-//                "Returns true if two bounding boxes have intersection.")
-//        .def("point_expand", (void (BoundingBox::*)(const Point&)) &BoundingBox::expand,
-//                "Expand bounding box to contain also given @p point.")
-//        .def("box_expand", (void (BoundingBox::*)(const BoundingBox&)) &BoundingBox::expand,
-//                "Expand bounding box to contain also given @p point.");
+    py::class_<BoundingBox>(m, "AABB")
+        .def(py::init<const vector<Point> &>())
+        .def("min", (const Point&(BoundingBox::*)() const) &BoundingBox::min , py::return_value_policy::reference)  // need conversion to numpy array
+        .def("max", (const Point&(BoundingBox::*)() const) &BoundingBox::max , py::return_value_policy::reference)  // need conversion to numpy array
+        .def("size", &BoundingBox::size)
+        .def("center", &BoundingBox::center) /// return Point, must convert to numpy array
+        .def("projection_center", &BoundingBox::projection_center,
+                "Return center of projection of the bounding box to given @p axis."
+                "Axis coding is: 0 - axis x, 1 - axis y, 2 - axis z.")
+        .def("contains_point", &BoundingBox::contains_point,        // need conversion of argument
+                "Returns true is the  box element contains point.")
+
+        .def("intersect", &BoundingBox::intersect,                  // hopefully correct
+                "Returns true if two bounding boxes have intersection.")
+        .def("add_point", (void (BoundingBox::*)(const Point&)) &BoundingBox::expand,
+                "Expand bounding box to contain also given @p point.")
+        .def("add_box", (void (BoundingBox::*)(const BoundingBox&)) &BoundingBox::expand,
+                "Expand bounding box to contain also given @p point.");
 
 
 //        /**
@@ -75,8 +99,6 @@ PYBIND11_PLUGIN(bih) {
 //            }
 //        }
 
-
-
 //        /**
 //         * Return index of the axis in which the box has longest projection.
 //         */
@@ -88,10 +110,16 @@ PYBIND11_PLUGIN(bih) {
 //       }
 
 
-//    py::class_<BIHTree>(m, "BIHTree")
-//        .def(py::init<unsigned int>(), py::arg("soft_leaf_size_limit")  = 20);
-//        //.def("setName", &Pet::setName)
-//        //.def("getName", &Pet::getName);
+
+
+    py::class_<BIHTree>(m, "BIH")
+        .def(py::init<unsigned int>(), py::arg("soft_leaf_size_limit")  = 20)
+        .def("add_boxes", &BIHTree::add_boxes)
+        .def("construct", &BIHTree::construct)
+        .def("find_box", &tree_find_box,
+                 py::arg("box"),  py::arg("full_list") = false)
+        .def("find_point", &tree_find_point,
+                 py::arg("point"),  py::arg("full_list") = false);
     return m.ptr();
    
 }
--- src/test_bih_tree.py
+++ src/test_bih_tree.py
@@ -2,14 +2,35 @@ import sys
 build_path="/home/jb/workspace/bih/build"
 sys.path+=[build_path]
 
-#import os
+import bih
+import numpy as np
 
-#bih_file = os.path.join(build_path,"bih")
-#bih_so_file = os.path.join(build_path,"bih.so")
-#assert( os.path.isfile(os.path.join(build_path,"bih")) )
-#import shutil
-#shutil.copyfile(bih_file, bih_so_file)
+points = np.array([[0.0, 0.0, 0.0]])
+points = [[0.0, 0.0, 0.0], [1,1,1], [-1, 2, 0]]
 
-import bih as m
-assert m.add(1, 2) == 3
+# Test basic AABB methods
+box=bih.AABB(points)
+assert box.min() == [-1, 0, 0]
+assert box.max() == [1, 2, 1]
+assert box.center() == [0, 1, 0.5]
+box.add_point([3,4,5])
+assert box.max() == [3,4,5]
 
+
+#Test basic BIH methods
+tree = bih.BIH()
+
+
+coords = [ 0, 0.5, 1, 1.5, 2]
+
+boxes = [ bih.AABB( [[p,0,0], [p+1, 1,1]]) for p in coords ]
+tree.add_boxes( boxes )
+tree.construct()
+intersect_box_ids = tree.find_point([0.7, 0.5, 0.5])
+assert intersect_box_ids == [0, 1]
+
+box = bih.AABB([[0.7, 0.5, 0.5], [1.3, 0.5,0.5]])
+intersect_box_ids = tree.find_box(box)
+assert intersect_box_ids == [0, 1, 2]
+
+print("Success.")
